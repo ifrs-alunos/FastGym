0d86f81e395ad8c1cf0095ba95bd6ac7da7601c0
Eduardo Chedid Padilha Ribeiro <eduardo@debian.Eduardo>
2019-10-23 11:35:52 -0300

    Atualizar

--- unchained/clients/urls.py
+++ unchained/clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import base, login
+from .views import base, login, cadastro
 
 urlpatterns = [
 	path('', base, name="base"),
-	path('login/', login, name="login")
+	path('login/', login, name="login"),
+	path('cadastro/', cadastro, name="cadastro")
 ]
--- unchained/clients/views.py
+++ unchained/clients/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
-
 def base(request):
 	return render(request, "clients/base.html", {})
 
 def login(request):
-	return render(request, "clients/login.html", {})
+	return render(request, "clients/login.html", {'page_title':'Login'})
+
+def cadastro(request):
+	print(request.method)
+	if request.method=="POST":
+		return render(request, "clients/login.html", {'page_title':'Login'})
+	return render(request, "clients/cadastro.html", {'page_title':'Cadastro'})
 
 #def base(request):
 #	return render(request, "clients/base.html", {})
